f4699c21861cd61988c5d6861f93b6e31d8057a9
ThomasJanko <thomas78125@gmail.com>
2026-04-10 10:16:40 +0200

    (fix): KznTokenAdresse

--- kzn-staking-backend/scripts/deploy-staking.ts
+++ kzn-staking-backend/scripts/deploy-staking.ts
@@ -6,7 +6,7 @@ const { ethers } = await network.connect({
 })
 
 async function main() {
-  const kznTokenAddress = "0x37b74A99503C17c62a64854E770978119192D6E3";
+  const kznTokenAddress = "0x22Dec6187dA597fFbF6aa28087b66DbbE27dEb16";
   const aprBps = 1200;
   const [deployer] = await ethers.getSigners();
 
